c46eeb652fcb606867f40e6ca05c56fb8a878ab7
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-25 23:54:59 +0530

    Mod

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World my name is rachit");
+            Console.WriteLine("None");
         }
     }
 }
